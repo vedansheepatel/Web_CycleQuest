423b7b0c50412eab5494f7ae25b83a5ba1275fd0
Vedanshee <v267pate@uwaterloo.ca>
2023-10-01 18:04:47 -0400

    infoModal added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,18 @@ var modal = document.getElementById('info_modal');
 var info = document.getElementById('info_button');
 var span = document.getElementsByClassName("close")[0];
 
-// Open the modal when the button is clicked
+// Open the modal when the button is clickedc
+
 info.addEventListener('click', () => {
+    console.log("Button clicked");
     modal.style.display = 'block';
 });
+
 
 // Close the modal when the close button is clicked
 span.addEventListener('click', () => {
     modal.style.display = 'none';
 });
+
+
 });
