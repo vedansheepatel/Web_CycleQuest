1dea3603dd16e59687c3a061e13cd90a5e59e288
Vedanshee <v267pate@uwaterloo.ca>
2023-10-05 09:57:15 -0400

    Make obstacles fall

--- assets/js/game1.js
+++ assets/js/game1.js
@@ -7,12 +7,46 @@ var player_position = player.offsetWidth;
 var moveSpeed=10;
 var currentpos = (screenWidth - player_position) / 2;
 
-if (player) {
-    const offsetWidth = player.offsetWidth;
-    console.log(`Offset Width: ${offsetWidth}px`);
-} else {
-    console.log('Image not found.');
+//Function to animate obstacles
+function animateObstacles() {
+    const obstacles = document.querySelectorAll('.obstacle');
+
+    obstacles.forEach((obstacle) => {
+        let topPosition = -100; // Initialize top position just above the screen
+        const fallSpeed = Math.random() * 4 + 2; // Randomize fall speed
+
+        // Function to update obstacle's position
+        function updatePosition() {
+            topPosition += fallSpeed;
+            obstacle.style.top = topPosition + 'px';
+
+            // If the obstacle is out of the screen, hide it
+            if (topPosition >= window.innerHeight) {
+                obstacle.style.display = 'none';
+
+            } else {
+                obstacle.style.display = 'block';
+            }
+
+            // If the obstacle is hidden, reset it to the top
+            if (topPosition > window.innerHeight + 50) {
+                topPosition = -100; // Position it just above the screen
+                obstacle.style.left = Math.random() * (window.innerWidth - 50) + 'px'; // Randomize horizontal position
+            }
+
+            // Request the next animation frame
+            requestAnimationFrame(updatePosition);
+        }
+
+        // Start the animation loop with a small delay
+        setTimeout(updatePosition, Math.random() * 1000); // Delay between 0 and 1000 milliseconds
+    });
 }
+
+// Call the animation function
+animateObstacles();
+
+
 
 function moveLeft(){
     currentpos -= moveSpeed;
@@ -39,6 +73,10 @@ document.addEventListener('keydown', (event) => {
         moveLeft();
     }
 });
+
+
+
+
 
 
 
